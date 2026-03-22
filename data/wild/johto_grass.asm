3ef248638ecc36b6f638b7561caafea4babe8fed
avalovebug <96801630+avalovebug@users.noreply.github.com>
2026-03-22 00:00:09 -0400

    Revert "added mareep to route 42"
    This reverts commit d9d1f63ad513830a4540b799f6e8fd8f56b4e4e7.

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1548,7 +1548,7 @@ JohtoGrassWildMons:
 	db 13, EKANS
 	db 14, SPEAROW
 	db 15, RATTATA
-	db 13, MAREEP
+	db 16, RATICATE
 	db 15, ARBOK
 	db 16, FEAROW
 	db 16, FEAROW
@@ -1556,7 +1556,7 @@ JohtoGrassWildMons:
 	db 13, EKANS
 	db 14, SPEAROW
 	db 15, RATTATA
-	db 13, MAREEP
+	db 16, RATICATE
 	db 15, ARBOK
 	db 16, FEAROW
 	db 16, FEAROW
